e09ac2262e866f5219ec9788b72a7c699c239f51
MustafaTRKYLMZ <cana66331@gmail.com>
2024-04-09 11:38:08 +0200

    homework is done

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,26 +1,18 @@
-import fetch from "node-fetch";
-import { dirname } from "path";
-import { fileURLToPath } from "node:url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-//st up express express from "express";
 import express from "express";
-import path from "path";
+
 const app = express();
-//console.log("dir name", __dirname);
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   console.log(req);
-  // res.sendFile(path.join(dirname, "public", "index.html"));
-  res.send("hello from ......>>>>> to frontend!");
+  res.send("hello from backend to frontend!");
 });
 
-// app.post("/weather", async (req, res) => {
-//   const cityName = req.body.cityName;
-//   app.use(express.json(cityName));
-
-//   res.send(cityName );
-// });
+app.post("/weather", async (req, res) => {
+  const cityName = req.body.cityName;
+  res.setHeader("Content-Type", "application/json");
+  res.send(cityName);
+});
 // listen to por t
 const PORT = 4200;
 
