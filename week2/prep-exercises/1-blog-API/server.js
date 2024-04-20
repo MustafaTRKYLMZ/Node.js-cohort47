5e48d3c16ea3378a937697f5c31370033a087f5c
MustafaTRKYLMZ <cana66331@gmail.com>
2024-04-20 22:42:47 +0200

    blog created

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,62 @@
-const express = require('express')
+const express = require("express");
 const app = express();
- 
 
 // YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+// Creating new posts
+app.post("/blogs", express.json(), (req, res) => {
+  const { title, content } = req.body;
+  fs.writeFileSync(`${title}.txt`, content);
+  res.end("ok");
+});
+
+//Updating existing posts
+app.put("/posts/:title", express.json(), (req, res) => {
+  const { title } = req.params;
+  const { content } = req.body;
+
+  if (fs.existsSync(`${title}.txt`)) {
+    fs.writeFileSync(`${title}.txt`, content);
+    res.send("ok");
+  } else {
+    res.status(404).send("This post does not exist!");
+  }
+});
+
+// Deleting posts
+app.delete("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+
+  if (fs.existsSync(`${title}.txt`)) {
+    fs.unlinkSync(`${title}.txt`);
+    res.send("ok");
+  } else {
+    res.status(404).send("This post does not exist!");
+  }
+});
+
+// Reading posts
+app.get("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+
+  if (fs.existsSync(`${title}.txt`)) {
+    const postContent = fs.readFileSync(`${title}.txt`, "utf8");
+    res.send(postContent);
+  } else {
+    res.status(404).send("This post does not exist!");
+  }
+});
+
+//Bonus/ read all posts
+app.get("/blogs", (req, res) => {
+  const files = fs.readdirSync(__dirname);
+  const blogPosts = files
+    .filter((file) => file.endsWith(".txt"))
+    .map((file) => ({ title: file.slice(0, -4) }));
+  res.json(blogPosts);
+});
+
+app.listen(3000);
