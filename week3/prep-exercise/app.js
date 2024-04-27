b7daba09432c05d9ea7aaa6c6e4ef07fafeb10e6
MustafaTRKYLMZ <cana66331@gmail.com>
2024-04-27 12:04:40 +0200

    prep-exercises done

--- week3/prep-exercise/app.js
+++ week3/prep-exercise/app.js
@@ -1,12 +1,84 @@
 import express from "express";
+import jsonwebtoken from "jsonwebtoken";
+import crypto from "crypto";
+import { verifyToken } from "./users.js";
 // Use below import statement for importing middlewares from users.js for your routes
-// import { ....... } from "./users.js";
+import { register, getSessionId, login, profile } from "./users.js";
+import { hash, compare } from "bcrypt";
 
 let app = express();
+app.use(register, login, profile);
 
 app.use(express.json());
-// Create routes here, e.g. app.post("/register", .......)
+const usersDatabase = [];
+const sessions = {};
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Create routes here, e.g. app.post("/register", .......)
+app.post("/register", async (req, res) => {
+  // new user
+  const saltRounds = 12;
+  const hashedPassword = await hash(secretPassword, saltRounds);
+
+  const newUser = {
+    username: req.body.username,
+    password: hashedPassword,
+  };
+  // add new user to database
+  usersDatabase.push(newUser);
+
+  // Send HTTP 201 with the newly created user. We do not send the password!
+  res.status(201).send({ username: newUser.username }).end();
+});
+
+app.get("/", (req, res) => {
+  console.log("Request received at: get method", Date.now());
+  res.send("Hello World!");
+});
+
+const login = async (username, password) => {
+  const user = usersDatabase.find((user) => user.username === username);
+  if (!user) {
+    return false;
+  }
+};
+
+// login route
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const sessionId = crypto.randomUUID();
+  sessions[sessionId] = username;
+
+  const loggedIn = await login(username, password);
+  if (loggedIn) {
+    res.status(200).send("Login successful").json({ sessionId }).end();
+  } else {
+    res.status(401).send("Login failed");
+  }
+});
+// logout
+app.post("/logout", async (req, res) => {
+  const sessionId = getSessionId(req);
+
+  if (!sessions[sessionId]) {
+    res.status(401).json({ message: "Not logged in" }).end();
+    return;
+  }
+
+  delete sessions[sessionId];
+
+  res.status(204).json().end();
+});
+
+// get profile
+app.get("/profile", async (req, res) => {
+  const sessionId = getSessionId(req);
+  const { username } = req.body.username;
+  const message = `Hello, you are logged in as ${username}!`;
+  res.status(200).json({ message }).end();
+});
+
+const PORT = 4201;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- week3/prep-exercise/users.js
+++ week3/prep-exercise/users.js
@@ -1,4 +1,82 @@
-// Create middlewares required for routes defined in app.js
-// export const register = async (req, res) => {};
+import jsonwebtoken from "jsonwebtoken";
+//login function
+const SECRET = "H6AIgu0wsGCH2mC6ypyRubiPoPSpV4t1";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxMjMiLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNzA3NDI4NDI3fQ.Fc1h3X2xqFspbBS3UdXTNEpd8dsL10Pt_lzAgnFZYAY";
 
-// You can also create helper functions in this file to help you implement logic inside middlewares
+const isValidUser = (user) => {
+  if (!user.username || !user.password || !user.email) {
+    return false;
+  }
+  return true;
+};
+
+// get session id
+export const getSessionId = (req) => {
+  const authorizationHeader = req.headers["authorization"];
+  if (!authorizationHeader) {
+    return null;
+  }
+  const sessionId = authorizationHeader.replace("Bearer ", "");
+  return sessionId.trim();
+};
+
+// register middleware
+export const register = async (req, res, next) => {
+  //create new user
+  const newUser = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  //check validation
+  const user = isValidUser(newUser);
+  if (!user) {
+    res.status(400).send("Invalid user");
+    return;
+  }
+  next();
+};
+// login middleware
+export const login = (req, res, next) => {
+  const { username, password } = req.body;
+  const checkUser = checkPassword(username, password);
+  if (!checkUser) {
+    res
+      .status(401)
+      .json({ message: "Invalid username - password combination" })
+      .end();
+    return;
+  }
+  // create token
+  const userToken = { username, isAdmin: false };
+  const token = jsonwebtoken.sign(userToken, SECRET);
+
+  // verify token
+  try {
+    const decodeUser = jsonwebtoken.verify(token, SECRET);
+    if (!decodeUser) {
+      res.status(401).send("Token verification failed").end();
+    }
+  } catch (error) {
+    res.status(401).send("Token verification failed").end();
+  }
+  next();
+};
+export const verifyToken = (token) => {
+  try {
+    return jsonwebtoken.verify(token, SECRET);
+  } catch (error) {
+    return null;
+  }
+};
+// profile middleware
+export const profile = (req, res, next) => {
+  const sessionId = getSessionId(req);
+  const decodedUser = verifyToken(sessionId);
+
+  if (!decodedUser) {
+    res.status(401).json({ message: "Not logged in" }).end();
+    return;
+  }
+  next();
+};
